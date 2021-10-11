d6c0e4d628d9ca2217ae0d5b07f3a0b6c150d1cf
mralan明 <904553738@qq.com>
2021-10-11 16:00:27 +0800

    更改版本号为1.0.1

--- buildSrc/src/main/kotlin/com/alan/mvvm/buildsrc/ProjectBuildConfig.kt
+++ buildSrc/src/main/kotlin/com/alan/mvvm/buildsrc/ProjectBuildConfig.kt
@@ -11,8 +11,8 @@ object ProjectBuildConfig {
     const val applicationId = "com.moment.ring"
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.0.1"
     const val isAppMode = false
 
     /**
